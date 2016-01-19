c66aea396152c990f42c8f614417357d02a35eeb
Ricardo Mota <ricardosllm@gmail.com>
2016-01-19 20:26:05 +0800

    Got bucket name and key from event map

--- src/fda_engine/core.cljs
+++ src/fda_engine/core.cljs
@@ -13,17 +13,21 @@
       (.readFileSync "static/config.edn" "UTF-8")
       read-string))
 
-(defn download [input]
-  (print input))
+(defn download [bucket-name key]
+  (print (str "bucket:  s3://" bucket-name "/" key)))
 
 (def ^:export core
   (async-lambda-fn
-   (fn [{:keys [magic-word] :as input} context]
-     (print (config :src-bucket-url))
-     (print context)
-     (if (not= magic-word (config :magic-word))
-       ;; We can fail/succeed wherever w/ fail!/succeed! - we can also
-       ;; leave an Error instance on the channel we return -
-       ;; see :delayed-failure above.
-       (lambda/fail! context "Your magic word is garbage")
-       (download input)))))
+    (fn [event context]
+      (let [bucket (((first (event :Records)) :s3) :bucket)
+            object (((first (event :Records)) :s3) :object)]
+        (print context)
+        (download (bucket :name) (object :key))
+        (if (not= event (config :magic-word))
+          ;; We can fail/succeed wherever w/ fail!/succeed! - we can also
+          ;; leave an Error instance on the channel we return -
+          ;; see :delayed-failure above.
+          (lambda/fail! context "Your magic word is garbage"))))))
+
+
+
