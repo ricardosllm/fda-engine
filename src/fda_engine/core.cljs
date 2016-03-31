97600c794d95f0023f40aea8a3bd5136ea8a4504
Ricardo Mota <ricardosllm@gmail.com>
2016-04-01 08:42:39 +0900

    Refactoring

--- src/fda_engine/core.cljs
+++ src/fda_engine/core.cljs
@@ -40,7 +40,6 @@
              (download input context)))
 
 
-
 ; entry function puts on download channel
 ; download function gets from download channel and download the image from S3
 ; download function puts it on the save channel
