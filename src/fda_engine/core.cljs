d0a2ff07594cfbac2d7d2389e8c0d681418b5107
Ricardo Mota <ricardosllm@gmail.com>
2016-04-08 08:24:20 +0800

    Refactoring

--- src/fda_engine/core.cljs
+++ src/fda_engine/core.cljs
@@ -43,12 +43,16 @@
 ; entry function puts on download channel
 ; download function gets from download channel and download the image from S3
 ; download function puts it on the save channel
+
 ; save function gets job from save channel and saves the photo to S3
 ; save function puts a job on the detect channel
+
 ; detect funtion gets the job from the detect channel and runs face detection algorithm
 ; detect function puts a upload job on the upload channel
+
 ; upload function gets the job from the upload channel and uploads the photo to S3
 ; upload function puts a done msg on the finish channel
+
 ; finish function gets the done msg from the finish channel and calls context.done
 
 
